88803e8dc5f16adf87632aaeb1f722c67ec6fde8
Aybek <turk.883ar@gmail.com>
2023-02-07 06:22:52 -0800

    add function GetById

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -7,14 +7,15 @@ import (
 	"net/http"
 )
 
-func GetAllArtists() ([]models.Artist, error) {
-	text, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+func GetAll(addr string) ([]models.Artist, error) {
+	text, err := http.Get(addr)
 	if err != nil {
 		return nil, err
 	}
 
 	artists := make([]models.Artist, 1)
 	defer text.Body.Close()
+
 	content, err := ioutil.ReadAll(text.Body)
 	if err != nil {
 		return nil, err
@@ -27,4 +28,27 @@ func GetAllArtists() ([]models.Artist, error) {
 	}
 
 	return artists, nil
+}
+
+func GetById(addr, id string) (*models.Artist, error) {
+	text, err := http.Get(addr + "/" + id)
+	if err != nil {
+		return nil, err
+	}
+	defer text.Body.Close()
+
+	artist := &models.Artist{}
+
+	content, err := ioutil.ReadAll(text.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(content, &artist)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return artist, nil
 }
