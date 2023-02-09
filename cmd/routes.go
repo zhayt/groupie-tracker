d90d02a1b4af0df84cbdcb2cb63ddd9206530525
Aybek <turk.883ar@gmail.com>
2023-02-09 06:41:14 -0800

    Add handler static files

--- cmd/routes.go
+++ cmd/routes.go
@@ -8,5 +8,6 @@ func (app *application) routes() http.Handler {
 	mux.Handle("/", app.onlyGetMethod(http.HandlerFunc(app.home)))
 	mux.Handle("/artist", app.onlyGetMethod(http.HandlerFunc(app.showArtist)))
 
+	mux.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./web/static"))))
 	return app.logRequest(mux)
 }
