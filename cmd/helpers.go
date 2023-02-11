a1dd43d36bf7f88268bf868dd0b7a6def15dda6d
Aybek <turk.883ar@gmail.com>
2023-02-11 06:28:07 -0800

    update handling error : show in page

--- cmd/helpers.go
+++ cmd/helpers.go
@@ -2,19 +2,44 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 	"runtime/debug"
 )
 
+type ErrorStatus struct {
+	Code    int
+	Message string
+}
+
 func (app *application) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
 	app.errorLog.Output(2, trace)
-
-	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	tmp, err := template.ParseFiles("./web/templates/error.html")
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusInternalServerError)
+	err = tmp.Execute(w, ErrorStatus{http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError)})
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
 }
 
 func (app *application) clientError(w http.ResponseWriter, status int) {
-	http.Error(w, http.StatusText(status), status)
+	tmp, err := template.ParseFiles("./web/templates/error.html")
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(status)
+	err = tmp.Execute(w, ErrorStatus{status, http.StatusText(status)})
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
 }
 
 func (app *application) notFound(w http.ResponseWriter) {
